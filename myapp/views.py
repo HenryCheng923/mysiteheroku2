3a6bb5cca0ae53914353f9571368e43e5872d9c1
Henry Cheng <account048924@gmail.com>
2019-10-09 14:23:45 +0800

    resect

--- myapp/views.py
+++ myapp/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request,'index', {})
+    return render(request,'index.html', {})
 def pylinkweb(request):
 	return HttpResponse("Django讓python能方便連結網頁")
 def deposits(request):
-	return render(request,'E_10_1',{})
+	return render(request,'E_10_1.html',{})
 def result(request):
 	pv=int(request.GET['amount'])
 	i=float(request.GET['rate'])
